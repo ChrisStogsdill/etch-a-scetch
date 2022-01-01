9708ab47f90903daf5db7ac0c2d3e38832ff085b
ChrisStogsdill <chris1stogsdill@gmail.com>
2021-12-31 19:12:48 -0600

    finish the project

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const outerContainer = document.querySelector('.outer-container');
 
+createEtchSquare(16);
+
 function createEtchSquare(gridSize) {
     let totalNumber = gridSize * gridSize;
     let  squareSize = 500 / gridSize;
+
+    
+    while (outerContainer.firstChild) {
+        outerContainer.removeChild(outerContainer.firstChild)
+    }
+    
+    
+    
 
     for (i = totalNumber; i > 0; i-- ){
     
@@ -11,12 +21,24 @@ function createEtchSquare(gridSize) {
                 width:${squareSize}px;
                 `}   
     let sketchSquares = document.querySelectorAll('.outer-container > div');
+
     for (i = 0; i < sketchSquares.length; i++ ) {
-        document.querySelectorAll('.outer-container > div')[i].addEventListener('mouseover', () => {
-            document.querySelectorAll('.outer-container > div')[i].classList.add('hovered');
+        document.querySelectorAll('.outer-container > div')[i].addEventListener('mouseover', (event) => {
+           event.target.classList.add('hovered');
+           
         })
     }
 
 }
 
-createEtchSquare(16);
+document.getElementById('reset-button').addEventListener('click', () => {
+    let userInput = prompt('Please enter a number between 1 and 100')
+    if (userInput < 0 || userInput > 100) {
+        alert('outside of range')
+    }
+    else {
+        createEtchSquare(userInput);
+    }
+})
+
+
